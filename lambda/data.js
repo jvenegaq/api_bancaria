fe38966f87159206a85c1877a5aee779a97a5898
jvenegaq <jevenegas1@utpl.edu.ec>
2024-07-05 01:44:59 -0500

    upgrade "stack" aws-ses

--- lambda/data.js
+++ lambda/data.js
@@ -1,6 +1,5 @@
 // data.js
 exports.handler = async (event) => {
-    // Simulando una respuesta de transacción con al menos 4 campos
     const transactionResponse = {
         transactionId: "123456",
         amount: 100.00,
--- lambda/db.js
+++ lambda/db.js
@@ -1,11 +1,9 @@
-// db.js
 const mysql = require('mysql2/promise');
-
 const connectToDatabase = async () => {
     return await mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: 'root',
+        password: 'Julio1981.',  
         database: 'mydb'
     });
 };
